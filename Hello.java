c3e992f828cb9c774f8bbe847f8ad4996fdf96d6
TrIsTaN-WeBb <142824907+TrIsTaN-WeBb@users.noreply.github.com>
2024-01-25 13:24:48 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**
+ * Hello
+ */
+
+ public class Hello {
+    public static void main(String[] args){
+        String msg = "Hello World";
+        String name = "Tristan Webb";
+        String dad = "Steve";
+        String mom = "Jessi";
+        String bro = "Dylan";
+        int age = 15;
+        System.out.println(msg);
+        System.out.println("My name is " + name + ", and I am " + age + " years old.");
+        System.out.println("My dad's name is " + dad + ".");
+        System.out.println("My mom's name is " + mom + ".");
+        System.out.println("My brother's name is " + bro + ".");
+    }
+ }
