56fabfb8020104bf2721ec8f4b2eee5f93886928
TrIsTaN-WeBb <142824907+TrIsTaN-WeBb@users.noreply.github.com>
2024-02-06 14:03:56 -0700

    Add files via upload

--- CountingArrays.java
+++ CountingArrays.java
@@ -4,20 +4,29 @@ public class CountingArrays {
         String type = "";
         int total = 0;
 
+        //to store the numbers from 1-100 inside of an array
         for (int i=1;i<=100;i++){
             array[i-1]=i;
         }
 
+        //to print out each number stored in the array, whether it is even or odd, and the overall total
         for (int i=0;i<array.length;i++){
+
+            //to check each number in the array for even or odd
             if (array[i]%2 == 0) {
                 type = "even";
             }else {
                 type = "odd";
             }
+
+            //to add each number in the array to a total
             total += array[i];
+
+            //to print the value stored in the array and whether it is even or odd
             System.out.println(array[i] + "\t" + type);
         }
         
+        //to print the total of all of the numbers in the array
         System.out.println("total = " + total);
     }
     
