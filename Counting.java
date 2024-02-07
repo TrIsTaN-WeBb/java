1e7bef4beb812cb2681d7d30f0ebbd2a4ae5c31d
TrIsTaN-WeBb <142824907+TrIsTaN-WeBb@users.noreply.github.com>
2024-02-07 11:51:49 -0700

    Add files via upload

--- Counting.java
+++ Counting.java
@@ -3,16 +3,24 @@ public class Counting {
         String type = "";
         int total = 0;
 
+        //to cycle through from 1 to 100, determine if the number is even or odd, print out the number and its type, and add it to a running total
         for (int i=1;i<=100;i++){
+
+            //to determine if the number is even or odd
             if (i%2 == 0) {
                 type = "even";
             }else {
                 type = "odd";
             }
+
+            //to add the number to a running total
             total = i + total;
+
+            //to print out the number and its type
             System.out.println(i + "\t" + type);
         }
         
+        //to print out the running total of all of the numbers
         System.out.println("total = " + total);
     }
     
